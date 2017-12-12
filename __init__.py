0785234969e675cd5babc31b176a9710fdfc1759
Raimon Esteve <raimon@nan-tic.com>
2017-12-12 10:30:52 +0100

    Remove Property fields issue2349

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 #This file is part country_zip module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from .configuration import *
-from .address import *
+from . import configuration
+from . import address
+
 
 def register():
     Pool.register(
-        Configuration,
-        Address,
+        configuration.Configuration,
+        configuration.ConfigurationCountry,
+        address.Address,
         module='country_zip', type_='model')
