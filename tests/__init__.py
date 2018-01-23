3a7215e64bd78d8b451a44c75d1362952f5b39d1
C?dric Krier <ced@b2ck.com>
2018-01-23 17:17:54 +0100

    Remove volumes on drone and fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
-#This file is part helloword module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-from .test_country_zip import suite
+# This file is part helloword module for Tryton.  The COPYRIGHT file at the top
+# level of this repository contains the full copyright notices and license
+# terms.
+try:
+    from trytond.modules.country_zip.tests.test_country_zip import suite
+except ImportError:
+    from .test_country_zip import suite
 
 __all__ = ['suite']
