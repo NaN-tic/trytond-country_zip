7431dba24400cf6c2b62a49dfa8898ba755eb554
C?dric Krier <ced@b2ck.com>
2018-01-23 17:18:06 +0100

    Fix style

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
-#This file is part country_zip module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part country_zip module for Tryton.  The COPYRIGHT file at the
+# top level of this repository contains the full copyright notices and license
+# terms.
 from trytond.model import ModelSQL, fields
 from trytond.model import ValueMixin
 from trytond.pool import PoolMeta
